2751ee029cc94d8ecadc1ce24eae4894b92db40c
Maanav Koladia <maanav.koladia@gmail.com>
2026-03-16 23:02:11 -0500

    bit logic was backwards

--- design/EverythingEverywhereAllAtOnce/tests/mem/bankFSM/tb_bankFSM.sv
+++ design/EverythingEverywhereAllAtOnce/tests/mem/bankFSM/tb_bankFSM.sv
@@ -14,7 +14,7 @@ module tb_bankFSM ();
         $vcdpluson;
     end
     
-    logic rst = 0;
+    logic rst;
 
     bank_fsm_controller_state_t fsm_state;
     logic [$clog2(
@@ -25,22 +25,24 @@ BANK_CONTROLLER_FSM_LOGIC_STATES
     logic mem_bank_controller_oe;
     logic mem_bank_controller_we;
     logic mem_bank_controller_send_store_address;
+    logic clearWB;
+    logic Precharged;
 
     bank_controller_fsm_logic uut0 (
         .clk(clk),
         .rst(rst),
         .ld_address_change_i(1'b0),
         .start_store_i(1'b0),
-        .S_0(mem_bank_controller_states_bits[0]),  // current-state bit 0
-        .S_1(mem_bank_controller_states_bits[1]),  // current-state bit 1
+        .S_0(mem_bank_controller_states_bits[4]),  // current-state bit 0
+        .S_1(mem_bank_controller_states_bits[3]),  // current-state bit 1
         .S_2(mem_bank_controller_states_bits[2]),  // current-state bit 2
-        .S_3(mem_bank_controller_states_bits[3]),  // current-state bit 3
-        .S_4(mem_bank_controller_states_bits[4]),  // current-state bit 4
+        .S_3(mem_bank_controller_states_bits[1]),  // current-state bit 3
+        .S_4(mem_bank_controller_states_bits[0]),  // current-state bit 4
         .st_addr_release_o(mem_bank_controller_send_store_address),
         .OE_o(mem_bank_controller_oe),
         .WE_o(mem_bank_controller_we),
-        .clear_writebufV_o(),
-        .PreCharged_o()
+        .clear_writebufV_o(clearWB),
+        .PreCharged_o(Precharged)
     );
 
     
