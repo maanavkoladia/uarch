d6bda55fde3514e575cfab3e9fd92376e67a7537
maanavkoladia <maanav.koladia@gmail.com>
2026-03-19 14:09:29 -0500

    got readmemh working

--- design/EverythingEverywhereAllAtOnce/tests/mem/mainMem/tb_mainMem_pkg.sv
+++ design/EverythingEverywhereAllAtOnce/tests/mem/mainMem/tb_mainMem_pkg.sv
@@ -1,13 +1,10 @@
 package tb_mainMem_pkg;
-    //10 ns
-    localparam CLK_PERIOD = 10 
 
-    task automatic DelayCLKs(
-        input logic cycles
-    );
+    // 10 ns
+    localparam CLK_PERIOD = 10;
 
+    task automatic DelayCLKs(input int cycles);
         #(CLK_PERIOD * cycles);
-
     endtask
-    
+
 endpackage
