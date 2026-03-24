500d27a48846d0001e5764e145ed293b0d1f08a6
maanavkoladia <maanav.koladia@gmail.com>
2026-03-23 19:23:55 -0500

    created a tb for dcache compilation

---
+++ design/EverythingEverywhereAllAtOnce/tests/DCache/dcache/tb_dcache.sv
@@ -0,0 +1,71 @@
+import common_pkg::*;
+import interconnect_pkg::*;
+import tb_dcache_pkg::*;
+
+module tb_dcache ();
+
+    `CLK_INIT(CLK_PERIOD)
+    //`GEN_WAVEFORM_VCD("wave.vcd", tb_memBanks, 10);
+    //`GEN_WAVEFORM_VPD("wave.vpd", tb_memBanks, 10);
+
+    logic rst;
+    wire [DATA_BUS_WIDTH_BITS - 1 : 0] dataBus;
+    wire [ADDRESS_BUS_WIDTH_BITS - 1 : 0] addrBus;
+
+    //to give the memmodule and addr
+    p_address_t addrForBus;
+    bool driveAddrBus;
+
+    logic [31 : 0] dataForBus;
+    bool driveDataBus;
+
+    core_2_dcache_t inFromCore;
+    dcache_2_core_t out2Core;
+    dte_2_dcache_t inFromDTE;
+    dcache_2_scheduler_t out2Sch;
+
+    initial begin
+        $vcdpluson;
+        $vcdplusmemon;
+    end
+
+    assign dataBus = driveDataBus ? dataForBus : 'z;
+    assign addrBus = driveAddrBus ? addrForBus : 'z;
+
+    DCache_TOP uut0 (
+        .clk(clk),
+        .rst(rst),
+        .inFromCore_i(),
+        .out2Core_o(),
+        .inFromDTE_i(),
+        .out2Sch_o(),
+        .dataBus(dataBus),
+        .address_bus(address_bus)
+    );
+    //init module to initMem
+    tb_memGen_InitRitual memLoader ();
+
+    initial begin
+        `LOG("DCache Tb Starting up");
+        rst = 0;  //actve low
+        inFromCore = '0;
+        out2Core = '0;
+        inFromDTE = '0;
+        out2Sch = '0;
+        driveAddrBus = 0;
+        driveDataBus = 0;
+        addrForBus = 0;
+        dataForBus = 0;
+
+        DelayCLKs(5);
+        rst = 1;  //actve low
+
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        //Extra completion time
+        /////////////////////////////////////////////////////////////////////////////////////
+        DelayCLKs(30);
+        `LOG("DCache  Tb Complete");
+        $finish;
+    end
+endmodule
