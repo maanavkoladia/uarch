191a280416e347912f42122b737a6d30f410e8c2
maanavkoladia <maanav.koladia@gmail.com>
2026-03-24 23:53:45 -0500

    git commit -m

---
+++ design/EverythingEverywhereAllAtOnce/rtl/ICache/ICache_DataStore.sv
@@ -0,0 +1,1 @@
+module ICache_DataStore
---
+++ design/EverythingEverywhereAllAtOnce/rtl/ICache/ICache_TagStore.sv
@@ -0,0 +1,75 @@
+module ICache_TagStore (
+    input wire clk,
+    input wire rst,
+
+    input wire en,  //active high
+    input v_address_t v_addr_i,
+    input p_address_t p_addr_i,
+    input bool ld_From_I_VC_Swap,
+    input bool LD_IC_SWAP_BUF,
+    input bool fill3_i,
+    input bool busy,
+    input swap_buf_t I_VC_SwapBuf_i,
+
+    output [ICACHE_TAG_WIDTH - 1:0] currTag_o,
+    output bool currLine_V
+);
+
+    localparam int NUM_CELLS = 2;
+
+    //
+    logic [ICACHE_INDEX_WIDTH - 1 : 0] v_addr_i_index = v_addr_i[ICACHE_INDEX_UB : ICACHE_INDEX_LB];
+    logic [ICACHE_INDEX_WIDTH - 1 : 0] v_addr_i_tag = v_addr_i[ICACHE_TAG_UB : ICACHE_TAG_LB];
+    //logic [ICACHE_INDEX_WIDTH - 1 : 0] swapBuf_p_addr_i_index = I_VC_SwapBuf_i.lineAddr[ICACHE_INDEX_UB : ICACHE_INDEX_LB];
+
+    bool validStore[NUM_ICACHE_LINES];
+
+    //comes from paddr_i or vswap addr, but bc of timing, well use the v_addr
+    //in always, it will always be correct
+    logic [ICACHE_INDEX_WIDTH - 1 - 1 : 0] ADDRESS_2_TagStore;
+    assign ADDRESS_2_TagStore = v_addr_i_index[ICACHE_INDEX_WIDTH-1-1 : 0];
+    logic tagCellOutSel = v_addr_i_index[ICACHE_INDEX_WIDTH-1];
+
+    //comes from p_addr upper index bit
+
+    //index with upper idx bit from p_addr and use if fill0
+    //cases to write is
+    //reading from swap buf,
+    //or from bus, in which case the
+    //address comes from p_addr
+    logic WR_2_TagStore[NUM_CELLS];
+    assign WR_2_TagStore[0] = !tagCellOutSel && (fill3_i || ld_From_I_VC_Swap) && en ? 0 : 1;
+    assign WR_2_TagStore[1] = tagCellOutSel && (fill3_i || ld_From_I_VC_Swap) && en ? 0 : 1;
+
+    logic [ICACHE_TAG_WIDTH -1 : 0] DIN_2_TagStore;
+    assign DIN_2_TagStore = v_addr_i_tag;
+
+    logic OE_2_TagStore = !busy || LD_IC_SWAP_BUF;
+
+
+    logic [7 : 0] DOUT_2_TagStore_extended[2];
+    logic [ICACHE_TAG_WIDTH - 1 : 0] DOUT_2_TagStore = DOUT_2_TagStore_extended[tagCellOutSel][ICACHE_TAG_WIDTH - 1: 0];
+
+    ram8b8w$ tag_store_ramCell_Lower (
+        .A(ADDRESS_2_TagStore),
+        .WR(WR_2_TagStore[0]),
+        .DIN({1'b0, DIN_2_TagStore}),
+        .OE(OE_2_TagStore),
+        .DOUT(DOUT_2_TagStore_extended[0])
+    );
+
+    ram8b8w$ tag_store_ramCell_Upper (
+        .A(ADDRESS_2_TagStore),
+        .WR(WR_2_TagStore[1]),
+        .DIN({1'b0, DIN_2_TagStore}),
+        .OE(OE_2_TagStore),
+        .DOUT(DOUT_2_TagStore_extended[1])
+    );
+
+    always_ff @(posedge clk) begin
+        if (!rst) validStore <= '0;
+        else if (fill3_i || ld_From_I_VC_Swap) validStore[v_addr_i_index] <= 1;
+    end
+
+endmodule
+
