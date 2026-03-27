46050f4a08e80a691a2f6da8820fee8ea23c7922
Jacob Joswin Erattuparambil <je28497@peach.ece.utexas.edu>
2026-03-27 14:01:58 -0500

    Starting work on fus

--- design/EverythingEverywhereAllAtOnce/rtl/core/EXE/FunctionalUnits/not.sv
+++ design/EverythingEverywhereAllAtOnce/rtl/core/EXE/FunctionalUnits/not.sv
@@ -4,31 +4,30 @@
 
 import common_pkg::*;
 
-module not_op (
-    input  uint64_t operand,
-    input  logic [1:0] data_size,  // 00=8bit, 01=16bit, 10=32bit
-    
-    output uint64_t result
-);
+module not(
+    input  uint32_t srA,
+    input logic[1:0] data_size,
+    output uint32_t dr_o,
+    output uint32_t res_buf_o,
+    output uint32_t flags
+); 
 
-    always_comb begin
-        case (data_size)
-            2'b00: begin  // 8-bit NOT
-                result = {56'h0, ~operand[7:0]};
-            end
-            
-            2'b01: begin  // 16-bit NOT
-                result = {48'h0, ~operand[15:0]};
-            end
-            
-            2'b10: begin  // 32-bit NOT
-                result = {32'h0, ~operand[31:0]};
-            end
-            
-            default: begin
-                result = 64'h0;
-            end
-        endcase
-    end
+    bool ld_16;
+    bool ld_32; 
+
+    uint32_t out_32;
+
+    assign out_32 = ~srA;
+    assign ld_16 = data_size[0] | data_size[1];
+    assign ld_32 = data_size[1];
+
+
+    assign dr_o[7:0] = out_32[7:0];
+    assign dr_o[15:8] = ld_16 ? out_32[15:7] : srA[15:8];
+    assign dr_o[31:16] = ld_32 ? out_32[31:16] : srA[31:16];
+
+    assign flags = '0;
+
+
 
 endmodule
