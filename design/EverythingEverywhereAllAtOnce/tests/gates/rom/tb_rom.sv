211b7b1a2449cad07bbe5d283f35688cabaa7bbf
Maanav Koladia <maanav.koladia@gmail.com>
2026-04-09 16:26:28 -0500

    rom tb up and running

--- design/EverythingEverywhereAllAtOnce/tests/gates/rom/tb_rom.sv
+++ design/EverythingEverywhereAllAtOnce/tests/gates/rom/tb_rom.sv
@@ -2,7 +2,7 @@
 
 module tb_rom ();
 
-    localparam int Clk_PERIOD = 11;
+    localparam int Clk_PERIOD = 10;
 
     task automatic DelayClks(input int cycles);
         #(Clk_PERIOD * cycles);
@@ -18,22 +18,30 @@ module tb_rom ();
     logic OE;
     logic [63 : 0] dout;
 
-    // read in mem
-    initial begin
-        readmemh("tb_rom.uut0.mem",uut0);
-    end
-
-    initial begin
-        `LOG("Starting ROM tb");
-
      rom64b32w$ uut0 (
          .A(addr),
          .OE(OE),
          .DOUT(dout)
      );
 
+    // read in mem
+    initial begin
+        $readmemh("fakeData.hex", tb_rom.uut0.mem,);
+    end
+
+    initial begin
+        `LOG("Starting ROM tb");
+        addr = 6;
+        OE = 0;
+
+        DelayClks(3);
+        @(posedge clk);
+        OE = 1;
+        @(posedge clk);
+        OE = 0;
         DelayClks(30);
         `LOG("Done with ROM tb");
+        $finish;
     end
     // Test signals
 
