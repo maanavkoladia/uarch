d69060abc980e1ba28c64486fe927c8d3bbf1035
Harish Eakambaram Kalai <he3837@peach.ece.utexas.edu>
2026-04-18 18:50:02 -0500

    auto make push

--- design/EverythingEverywhereAllAtOnce/rtl/mem/Mem_Controller/gen/mem_controller_fsm.sv
+++ design/EverythingEverywhereAllAtOnce/rtl/mem/Mem_Controller/gen/mem_controller_fsm.sv
@@ -151,11 +151,11 @@ wire NS_2_t3;
 
 `OR_4(NS_2_or, 1, NS_2, NS_2_t0, NS_2_t1, NS_2_t2, NS_2_t3)
 
-// NS_3 = (S_0 & !S_1 & !S_2 & S_3) | (S_0 & S_1 & S_2 & !S_3) | (!S_0 & !S_1 & !S_2 & !S_3 & ld_req_i & write_req_i)
+// NS_3 = (S_0 & S_1 & S_2 & !S_3) | (S_0 & !S_1 & !S_2 & S_3) | (!S_0 & !S_1 & !S_2 & !S_3 & ld_req_i & write_req_i)
 wire NS_3_t0;
-`AND_4(NS_3_and0, 1, NS_3_t0, S_0, S_1_inv, S_2_inv, S_3)
+`AND_4(NS_3_and0, 1, NS_3_t0, S_0, S_1, S_2, S_3_inv)
 wire NS_3_t1;
-`AND_4(NS_3_and1, 1, NS_3_t1, S_0, S_1, S_2, S_3_inv)
+`AND_4(NS_3_and1, 1, NS_3_t1, S_0, S_1_inv, S_2_inv, S_3)
 wire NS_3_t2;
 `AND_6(NS_3_and2, 1, NS_3_t2, S_0_inv, S_1_inv, S_2_inv, S_3_inv, ld_req_i, write_req_i)
 
@@ -169,15 +169,15 @@ wire mem_ready_o_t1;
 
 `OR_2(mem_ready_o_or, 1, mem_ready_o, mem_ready_o_t0, mem_ready_o_t1)
 
-// set_ld_tristate_o = (S_0 & !S_2 & !S_3) | (S_1 & !S_2 & !S_3) | (!S_0 & !S_1 & S_2 & !S_3) | (!S_1 & S_2 & !S_3 & hit_i) | (!S_1 & !S_3 & ld_req_i & !write_req_i & hit_i)
+// set_ld_tristate_o = (S_0 & !S_2 & !S_3) | (S_1 & !S_2 & !S_3) | (!S_1 & S_2 & !S_3 & hit_i) | (!S_0 & !S_1 & S_2 & !S_3) | (!S_1 & !S_3 & ld_req_i & !write_req_i & hit_i)
 wire set_ld_tristate_o_t0;
 `AND_3(set_ld_tristate_o_and0, 1, set_ld_tristate_o_t0, S_0, S_2_inv, S_3_inv)
 wire set_ld_tristate_o_t1;
 `AND_3(set_ld_tristate_o_and1, 1, set_ld_tristate_o_t1, S_1, S_2_inv, S_3_inv)
 wire set_ld_tristate_o_t2;
-`AND_4(set_ld_tristate_o_and2, 1, set_ld_tristate_o_t2, S_0_inv, S_1_inv, S_2, S_3_inv)
+`AND_4(set_ld_tristate_o_and2, 1, set_ld_tristate_o_t2, S_1_inv, S_2, S_3_inv, hit_i)
 wire set_ld_tristate_o_t3;
-`AND_4(set_ld_tristate_o_and3, 1, set_ld_tristate_o_t3, S_1_inv, S_2, S_3_inv, hit_i)
+`AND_4(set_ld_tristate_o_and3, 1, set_ld_tristate_o_t3, S_0_inv, S_1_inv, S_2, S_3_inv)
 wire set_ld_tristate_o_t4;
 `AND_5(set_ld_tristate_o_and4, 1, set_ld_tristate_o_t4, S_1_inv, S_3_inv, ld_req_i, write_req_i_inv, hit_i)
 
