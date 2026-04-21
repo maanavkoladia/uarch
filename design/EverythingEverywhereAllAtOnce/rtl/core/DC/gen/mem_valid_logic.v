90ab2204a31293250c35476b68490b8ce2dafb34
Harish Eakambaram Kalai <he3837@peach.ece.utexas.edu>
2026-04-21 01:07:12 -0500

    jacob commit from harish bc he cant use AI for whatever reason

--- design/EverythingEverywhereAllAtOnce/rtl/core/DC/gen/mem_valid_logic.v
+++ design/EverythingEverywhereAllAtOnce/rtl/core/DC/gen/mem_valid_logic.v
@@ -104,15 +104,15 @@ wire WB_stall_i_inv;
 // SOP logic (Quine-McCluskey minimised)
 // ----------------------------------------------------------------
 
-// MEM_we_o = !MEM_V_i | (!MEM_stall_i & !EXE_V_i) | (!MEM_stall_i & !WB_stall_i)
+// MEM_we_o = !MEM_V_i | (!MEM_stall_i & !WB_stall_i) | (!MEM_stall_i & !EXE_V_i)
 wire MEM_we_o_t0;
 wire MEM_we_o_and0_buf_mid;
 `INV_N(MEM_we_o_and0_buf_i0, 1, MEM_V_i_inv, MEM_we_o_and0_buf_mid)
 `INV_N(MEM_we_o_and0_buf_i1, 1, MEM_we_o_and0_buf_mid, MEM_we_o_t0)
 wire MEM_we_o_t1;
-`AND_2(MEM_we_o_and1, 1, MEM_we_o_t1, MEM_stall_i_inv, EXE_V_i_inv)
+`AND_2(MEM_we_o_and1, 1, MEM_we_o_t1, MEM_stall_i_inv, WB_stall_i_inv)
 wire MEM_we_o_t2;
-`AND_2(MEM_we_o_and2, 1, MEM_we_o_t2, MEM_stall_i_inv, WB_stall_i_inv)
+`AND_2(MEM_we_o_and2, 1, MEM_we_o_t2, MEM_stall_i_inv, EXE_V_i_inv)
 
 `OR_3(MEM_we_o_or, 1, MEM_we_o, MEM_we_o_t0, MEM_we_o_t1, MEM_we_o_t2)
 
