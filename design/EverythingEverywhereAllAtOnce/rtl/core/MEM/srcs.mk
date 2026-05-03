31040613c0f913624914c046ddead45c98f9d1aa
Harish Eakambaram <harishthehershey@Harishs-MacBook-Air-8.local>
2026-05-03 04:03:42 -0500

    testing mem porting

--- design/EverythingEverywhereAllAtOnce/rtl/core/MEM/srcs.mk
+++ design/EverythingEverywhereAllAtOnce/rtl/core/MEM/srcs.mk
@@ -7,7 +7,9 @@ GEN_MEM_SRC_FILES = \
 
 #MEM source files
 MEM_STAGE_SRC_FILES = \
-	$(MEM_PATH_INTERNAL)/mem_miss_stall_logic.sv \
-	$(MEM_PATH_INTERNAL)/MEM.sv \
+	$(MEM_PATH_INTERNAL)/structural/mem_miss_stall_logic.sv \
+	$(MEM_PATH_INTERNAL)/structural/MEM.sv \
 
 MEM_STAGE_STRUCTURAL_SRC_FILES = \
+	$(MEM_PATH_INTERNAL)/structural/mem_miss_stall_logic_structural.v \
+	$(MEM_PATH_INTERNAL)/structural/MEM_structural.sv \
