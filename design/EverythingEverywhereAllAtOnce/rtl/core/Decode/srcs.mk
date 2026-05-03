1f64d305cd6b1f294f9fa44fb63e5ede685bc490
Harish Eakambaram Kalai <he3837@toad.ece.utexas.edu>
2026-05-02 21:10:26 -0500

    need some more files from other branch

--- design/EverythingEverywhereAllAtOnce/rtl/core/Decode/srcs.mk
+++ design/EverythingEverywhereAllAtOnce/rtl/core/Decode/srcs.mk
@@ -4,6 +4,8 @@ DECODE_PKGS = \
 						$(DECODE_PATH_INTERNAL)/pkg/Decode_pkg.sv \
 
 GEN_DECODE_SRC_FILES = \
+						  $(DECODE_PATH_INTERNAL)/../../defines/reg_ids_define.vh \
+						  $(DECODE_PATH_INTERNAL)/../../defines/control_store_define.vh \
 						  $(DECODE_PATH_INTERNAL)/structural/gen/MOD_LUT.v \
 						  $(DECODE_PATH_INTERNAL)/structural/gen/OP_LUT.v	\
 						  $(DECODE_PATH_INTERNAL)/structural/gen/PF_LUT.v \
@@ -14,17 +16,40 @@ GEN_DECODE_SRC_FILES = \
 						  $(DECODE_PATH_INTERNAL)/structural/gen/rr_valid_logic.v \
 
 DECODE_SRC_FILES_STRUCTURAL = \
-								$(DECODE_PATH_INTERNAL)/structural/disp_finder.v \
-								$(DECODE_PATH_INTERNAL)/structural/imm_finder.v \
+								$(DECODE_PATH_INTERNAL)/structural/disp_finder_structural.v \
+								$(DECODE_PATH_INTERNAL)/structural/imm_finder_structural.v \
 								$(DECODE_PATH_INTERNAL)/structural/modrm_size.v \
 								$(DECODE_PATH_INTERNAL)/structural/num_pf_gen.v \
 								$(DECODE_PATH_INTERNAL)/structural/op_size.v \
 								$(DECODE_PATH_INTERNAL)/structural/pf_checker.v \
 								$(DECODE_PATH_INTERNAL)/structural/pf_vector_gen.v \
-								$(DECODE_PATH_INTERNAL)/structural/ppu.v \
-								$(DECODE_PATH_INTERNAL)/structural/selection_logic.v \
-								$(DECODE_PATH_INTERNAL)/structural/sib_finder.v \
-								$(DECODE_PATH_INTERNAL)/structural/predecode.v \
+								$(DECODE_PATH_INTERNAL)/structural/ppu_structural.v \
+								$(DECODE_PATH_INTERNAL)/structural/selection_logic_structural.v \
+								$(DECODE_PATH_INTERNAL)/structural/sib_finder_structural.v \
+								$(DECODE_PATH_INTERNAL)/structural/predecode_structural.v \
+
+# GEN_DECODE_SRC_FILES = \
+# 						  $(DECODE_PATH_INTERNAL)/structural/gen/MOD_LUT.v \
+# 						  $(DECODE_PATH_INTERNAL)/structural/gen/OP_LUT.v	\
+# 						  $(DECODE_PATH_INTERNAL)/structural/gen/PF_LUT.v \
+# 						  $(DECODE_PATH_INTERNAL)/structural/gen/rep_fsm.sv \
+# 						  $(DECODE_PATH_INTERNAL)/structural/gen/rep_movs_fsm.sv \
+# 						  $(DECODE_PATH_INTERNAL)/structural/gen/rep_cmp_fsm.sv \
+# 						  $(DECODE_PATH_INTERNAL)/structural/gen/ir_logic.v \
+# 						  $(DECODE_PATH_INTERNAL)/structural/gen/rr_valid_logic.v \
+
+# DECODE_SRC_FILES_STRUCTURAL = \
+# 								$(DECODE_PATH_INTERNAL)/structural/disp_finder.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/imm_finder.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/modrm_size.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/num_pf_gen.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/op_size.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/pf_checker.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/pf_vector_gen.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/ppu.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/selection_logic.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/sib_finder.v \
+# 								$(DECODE_PATH_INTERNAL)/structural/predecode.v \
 
 DECODE_SRC_FILES = \
 						$(DECODE_PATH_INTERNAL)/br_info_processing.sv \
@@ -39,32 +64,3 @@ DECODE_SRC_FILES = \
 
 #dont add gen files here are they are includede in the core srcs.mk file
 DECODE_STRUCTURAL_SRC_FILES = \
-
-
-
-
-
-# GEN_DECODE_SRC_FILES = \
-# 						  $(DECODE_PATH_INTERNAL)/../../defines/reg_ids_define.vh \
-# 						  $(DECODE_PATH_INTERNAL)/../../defines/control_store_define.vh \
-# 						  $(DECODE_PATH_INTERNAL)/structural/gen/MOD_LUT.v \
-# 						  $(DECODE_PATH_INTERNAL)/structural/gen/OP_LUT.v	\
-# 						  $(DECODE_PATH_INTERNAL)/structural/gen/PF_LUT.v \
-# 						  $(DECODE_PATH_INTERNAL)/structural/gen/rep_fsm.sv \
-# 						  $(DECODE_PATH_INTERNAL)/structural/gen/rep_movs_fsm.sv \
-# 						  $(DECODE_PATH_INTERNAL)/structural/gen/rep_cmp_fsm.sv \
-# 						  $(DECODE_PATH_INTERNAL)/structural/gen/ir_logic.v \
-# 						  $(DECODE_PATH_INTERNAL)/structural/gen/rr_valid_logic.v \
-
-# DECODE_SRC_FILES_STRUCTURAL = \
-# 								$(DECODE_PATH_INTERNAL)/structural/disp_finder_structural.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/imm_finder_structural.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/modrm_size.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/num_pf_gen.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/op_size.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/pf_checker.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/pf_vector_gen.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/ppu_structural.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/selection_logic_structural.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/sib_finder_structural.v \
-# 								$(DECODE_PATH_INTERNAL)/structural/predecode_structural.v \
---
+++ design/EverythingEverywhereAllAtOnce/tests/Stages/Harish_StageTesting/config/VerySimpleTest/VerySimpleTest.s
@@ -0,0 +1,14 @@
+#define __CS__ 0x0000
+#define __DS__ 0x4002
+#define __SS__ 0xF000
+
+
+.org 0x00000000
+.code
+.global _start
+
+_start:
+    movb $1, %al
+    movl %eax, %eax
+    movl %eax, %eax
+    hlt
