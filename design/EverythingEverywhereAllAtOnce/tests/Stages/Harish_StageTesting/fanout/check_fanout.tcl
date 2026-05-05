15169d5800b38524f2a238e8dc467a3213fc5bb8
maanavkoladia <maanav.koladia@gmail.com>
2026-05-04 22:35:00 -0500

    fanout scripts

---
+++ design/EverythingEverywhereAllAtOnce/tests/Stages/Harish_StageTesting/fanout/check_fanout.tcl
@@ -0,0 +1,25 @@
+set target_library ""
+set link_library "*"
+
+
+set __top_module__  "AllAtOnce_TOP"
+set __masterfile__  "./master.txt"
+
+define_design_lib WORK -path ./WORK
+
+set f [open "$__masterfile__" r]
+set files [split [read $f] "\n"]
+close $f
+# strip blanks/comments
+set files [lsearch -all -inline -not -regexp $files {^\s*(#.*)?$}]
+
+read_file -format verilog [lindex $files 0]
+analyze -format verilog [lrange $files 1 end]
+elaborate $__top_module__
+
+current_design $__top_module__
+link
+
+source fanout.tcl
+fanout_check
+
